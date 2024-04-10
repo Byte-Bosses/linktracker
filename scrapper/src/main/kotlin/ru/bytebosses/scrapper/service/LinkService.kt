f9b3a992b522e4fddf00622bc7db65b05e5e5923
ArhostCode <e-gulyakin@mail.ru>
2024-04-10 21:46:06 +0300

    Add link model

--- scrapper/src/main/kotlin/ru/bytebosses/scrapper/service/LinkService.kt
+++ scrapper/src/main/kotlin/ru/bytebosses/scrapper/service/LinkService.kt
@@ -1,6 +1,7 @@
 package ru.bytebosses.scrapper.service
 
 import ru.bytebosses.scrapper.dto.response.LinkResponse
+import ru.bytebosses.scrapper.model.Link
 import java.net.URI
 import kotlin.time.Duration
 
@@ -8,6 +9,7 @@ interface LinkService {
     fun listLinks(tgChatId: Long): List<LinkResponse>
     fun addLink(link: URI, tgChatId: Long): LinkResponse
     fun removeLink(id: Long, tgChatId: Long): LinkResponse
-    fun listStaleLinks(limit: Int, minTimeSinceLastCheck: Duration)
+    fun listStaleLinks(limit: Int, minTimeSinceLastCheck: Duration): List<Link>
     fun getChatIdsForLink(id: Long): List<Long>
+    fun updateLink(link: Link)
 }
