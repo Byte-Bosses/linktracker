6ef525182f901248bff8980ea5682974207b7717
Nick-552 <TTchip552@ya.ru>
2024-04-10 21:22:21 +0300

    added methods to LinkService

--- scrapper/src/main/kotlin/ru/bytebosses/scrapper/service/LinkService.kt
+++ scrapper/src/main/kotlin/ru/bytebosses/scrapper/service/LinkService.kt
@@ -2,9 +2,12 @@ package ru.bytebosses.scrapper.service
 
 import ru.bytebosses.scrapper.dto.response.LinkResponse
 import java.net.URI
+import kotlin.time.Duration
 
 interface LinkService {
     fun listLinks(tgChatId: Long): List<LinkResponse>
     fun addLink(link: URI, tgChatId: Long): LinkResponse
     fun removeLink(id: Long, tgChatId: Long): LinkResponse
+    fun listStaleLinks(limit: Int, minTimeSinceLastCheck: Duration)
+    fun getChatIdsForLink(id: Long): List<Long>
 }
