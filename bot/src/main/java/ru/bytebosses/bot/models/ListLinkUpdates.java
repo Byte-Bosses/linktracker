a3817c8df95a1b240c76f0225b533f2304d411da
evgenij <chucknorristhegreat@yandex.ru>
2024-05-16 22:14:33 +0300

    deleted ListLinkUpdates.java from this package and moved ListLinksResponse.java to api.dto.response

--- bot/src/main/java/ru/bytebosses/bot/models/ListLinkUpdates.java
+++
@@ -1,7 +0,0 @@
-package ru.bytebosses.bot.models;
-
-import java.util.List;
-import ru.bytebosses.bot.api.dto.request.LinkUpdate;
-
-public record ListLinkUpdates(List<LinkUpdate> linkUpdates) {
-}
--- bot/src/main/java/ru/bytebosses/bot/models/ListLinksResponse.java
+++
@@ -1,5 +0,0 @@
-package ru.bytebosses.bot.models;
-
-import java.util.List;
-
-public record ListLinksResponse(List<Link> links) {}
