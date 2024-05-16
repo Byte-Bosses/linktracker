41dd45b8540dac4b724b3ebf73074ee86825c30b
evgenij <chucknorristhegreat@yandex.ru>
2024-05-16 15:24:58 +0300

    feat: Added configuration classes to EnableConfigurationProperties annotation

--- bot/src/main/java/ru/bytebosses/bot/BotApplication.java
+++ bot/src/main/java/ru/bytebosses/bot/BotApplication.java
@@ -1,12 +1,14 @@
 package ru.bytebosses.bot;
 
+import ru.bytebosses.bot.api.endpoints.controllers.rateLimit.RateLimiterAspect;
 import ru.bytebosses.bot.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.bytebosses.bot.configuration.RetryConfiguration;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationConfig.class)
+@EnableConfigurationProperties({ApplicationConfig.class, RateLimiterAspect.class, RetryConfiguration.class})
 public class BotApplication {
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
