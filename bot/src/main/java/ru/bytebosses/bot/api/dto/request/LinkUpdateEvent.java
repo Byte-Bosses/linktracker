80f17573697256579ff47d72fd194eadce113c0b
evgenij <chucknorristhegreat@yandex.ru>
2024-05-16 22:08:04 +0300

    fix: removed toString unnecessary overriding

--- bot/src/main/java/ru/bytebosses/bot/api/dto/request/LinkUpdateEvent.java
+++ bot/src/main/java/ru/bytebosses/bot/api/dto/request/LinkUpdateEvent.java
@@ -4,12 +4,4 @@ import java.time.OffsetDateTime;
 import java.util.Map;
 
 public record LinkUpdateEvent(String type, OffsetDateTime updateTime, Map<String, String> metaInfo) {
-    @Override
-    public String toString() {
-        return "LinkUpdateEvent{"
-            + "type='" + type + '\''
-            + ", updateTime=" + updateTime
-            + ", metaInfo=" + metaInfo
-            + '}';
-    }
 }
--- bot/src/main/java/ru/bytebosses/bot/api/dto/request/LinkUpdateInformation.java
+++ bot/src/main/java/ru/bytebosses/bot/api/dto/request/LinkUpdateInformation.java
@@ -4,13 +4,5 @@ import java.net.URI;
 import java.util.List;
 
 public record LinkUpdateInformation(URI uri, String title, List<LinkUpdateEvent> events) {
-    @Override
-    public String toString() {
-        return "LinkUpdateInformation{"
-            + "uri=" + uri
-            + ", title='" + title + '\''
-            + ", events=" + events
-            + '}';
-    }
 }
 
