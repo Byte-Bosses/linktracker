fa53af0ff4e887ea65be22656d52fcdec50f753e
ArhostCode <e-gulyakin@mail.ru>
2024-05-19 11:54:44 +0300

    refactor(scrapper): Remove unused code

--- scrapper/src/main/kotlin/ru/bytebosses/scrapper/configuration/extension/StackoverflowExtensionAutoConfiguration.kt
+++ scrapper/src/main/kotlin/ru/bytebosses/scrapper/configuration/extension/StackoverflowExtensionAutoConfiguration.kt
@@ -1,14 +1,11 @@
 package ru.bytebosses.scrapper.configuration.extension
 
-import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.bytebosses.extension.api.InformationProvider
 import ru.bytebosses.extension.stackoverflow.StackoverflowInformationProvider
 import ru.bytebosses.scrapper.provider.InformationProvidersRegistry
-import java.net.URI
-import java.time.OffsetDateTime
 
 @Configuration
 @ConditionalOnBean(InformationProvidersRegistry::class)
@@ -20,17 +17,5 @@ class StackoverflowExtensionAutoConfiguration {
             initialize()
             informationProvidersRegistry.registerInformationProvider("stackoverflow", this)
         }
-
-    @PostConstruct
-    fun init() {
-        StackoverflowInformationProvider().apply {
-            initialize()
-            println(retrieveInformation(
-                URI("https://stackoverflow.com/questions/1770076"),
-                hashMapOf(),
-                OffsetDateTime.now()
-            ))
-        }
-    }
 
 }
